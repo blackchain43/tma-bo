99d35f96520475fdd9507d77335fc51ca6c66b25
hqphuoc <hqphuoc@tmproxy.com>
2024-05-16 17:33:46 +0700

    add ton connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,51 @@
 import { useState } from "react";
 import "./App.css";
-
+import { THEME, TonConnectUIProvider } from "@tonconnect/ui-react";
+import { Header } from "./components/Header/Header";
 import WebApp from "@twa-dev/sdk";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <TonConnectUIProvider
+      manifestUrl="https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json"
+      uiPreferences={{ theme: THEME.DARK }}
+      walletsListConfiguration={{
+        includeWallets: [
+          {
+            appName: "safepalwallet",
+            name: "SafePal",
+            imageUrl:
+              "https://s.pvcliping.com/web/public_image/SafePal_x288.png",
+            aboutUrl: "https://www.safepal.com/download",
+            jsBridgeKey: "safepalwallet",
+            platforms: ["ios", "android", "chrome", "firefox"],
+          },
+          {
+            appName: "tonwallet",
+            name: "TON Wallet",
+            imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
+            aboutUrl:
+              "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
+            universalLink: "https://wallet.ton.org/ton-connect",
+            jsBridgeKey: "tonwallet",
+            bridgeUrl: "https://bridge.tonapi.io/bridge",
+            platforms: ["chrome", "android"],
+          },
+        ],
+      }}
+      actionsConfiguration={{
+        twaReturnUrl: "https://web.telegram.org/k/#@tmabobot",
+      }}
+    >
+      <Header />
       <h1>TWA + Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
-      {/*  */}
       <div className="card">
         <button
           onClick={() =>
@@ -24,7 +55,7 @@ function App() {
           Show Alert
         </button>
       </div>
-    </>
+    </TonConnectUIProvider>
   );
 }
 
